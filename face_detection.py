fe8ace9b6d23209efcd94c942b39d804eb9ac8b1
finteger <nashassociatesinc@gmail.com>
2024-10-09 15:35:54 -0600

    Update

--- face_detection.py
+++ face_detection.py
@@ -6,10 +6,37 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 #Load a sample picture and learn how to recognize it
-your_image = face_recognition.load_image_file("todd.jpg")
+your_image = face_recognition.load_image_file("ryan.png")
 your_face_encoding = face_recognition.face_encodings(your_image)[0]
 
 #Create an array of known face encodings
 known_face_encodings = [
     your_face_encoding,
 ]
+
+known_face_names = [
+    "Ryan"
+]
+
+#Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    #grab a single frame of the video
+    ret, frame = video_capture.read()
+    
+    #Only process every other frame
+    if process_this_frame:
+        #Resize frame of the video to 1/4 size for faster 
+        #face recognition processing
+        small_frame = cv2.resize(frame, (0,0), fx=0.25, y=0.25)
+        
+        #Convert the image from BGR (which OpenCV uses) to RGB (face_recognition uses)
+        rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
+        
+        #Find all the faces and facial encodings in the current frame of the video
+        face_locations = face_recogition.face_locations(rgb_small_frame)
+    
