0f26df2292b36ea949cc921412fc7d7c3dabc1e6
finteger <nashassociatesinc@gmail.com>
2024-10-10 15:08:04 -0600

    Update

--- face_detection.py
+++ face_detection.py
@@ -54,14 +54,41 @@ while True:
                 name = known_face_names[first_match_index]
                 
             #Or instead, use the known face with the smallest distance to the new face  
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)  
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding) 
+            #argmin grabs the floor value from array (smallest distance and return the in)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
             
             face_names.append(name)    
-              
+    
+    #skips every second frame          
     process_this_frame = not process_this_frame      
     
     #Display the results
-            
+    for(top, right, bottom, left), name in zip(face_locations, face_names):
+        #Scale back up face locations since the frame we detected in was scaled down to 1/4th size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+        
+        #Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 2)  
+        
+        #Draw a lavel with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0,0,255), cv2.FILLED)    
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)  
+        
+        
+    #Display the resulting image
+    cv2.imshow('Video', frame)
+    
+    #Hit 'q' on the keyboard to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
