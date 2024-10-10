69ddb80f4b9c439cad94954c4f0a57b65785d468
finteger <nashassociatesinc@gmail.com>
2024-10-10 14:04:15 -0600

    Update

--- face_detection.py
+++ face_detection.py
@@ -32,11 +32,29 @@ while True:
     if process_this_frame:
         #Resize frame of the video to 1/4 size for faster 
         #face recognition processing
-        small_frame = cv2.resize(frame, (0,0), fx=0.25, y=0.25)
+        small_frame = cv2.resize(frame, (0,0), fx=0.25, fy=0.25)
         
         #Convert the image from BGR (which OpenCV uses) to RGB (face_recognition uses)
         rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
         
         #Find all the faces and facial encodings in the current frame of the video
         face_locations = face_recognition.face_locations(rgb_small_frame)
-    
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            #See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            
+            name = "unknown"
+            
+            #If a match was found in known_face_encodings, just use the first one
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+                
+            #Or instead, use the known face with the smallest distance to the new face  
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)  
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
